55a8d38f35905549043a224e5242bec0e31c5c4d
Juli-Savalia <3382julisavalia@gmail.com>
2024-02-02 11:53:24 +0530

    update

--- DESKTOP/CALCULATOR/CALC/CALC/Form1.Designer.vb
+++ DESKTOP/CALCULATOR/CALC/CALC/Form1.Designer.vb
@@ -22,52 +22,404 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        GroupBox1 = New GroupBox()
+        BackgroundWorker1 = New ComponentModel.BackgroundWorker()
+        Panel1 = New Panel()
         TextBox1 = New TextBox()
-        MaskedTextBox1 = New MaskedTextBox()
-        GroupBox1.SuspendLayout()
+        Panel2 = New Panel()
+        Button24 = New Button()
+        Button22 = New Button()
+        Button21 = New Button()
+        Button20 = New Button()
+        Button19 = New Button()
+        Button18 = New Button()
+        Button17 = New Button()
+        Button16 = New Button()
+        Button15 = New Button()
+        Button14 = New Button()
+        Button13 = New Button()
+        Button12 = New Button()
+        Button11 = New Button()
+        Button10 = New Button()
+        Button9 = New Button()
+        Button8 = New Button()
+        Button7 = New Button()
+        Button6 = New Button()
+        Button5 = New Button()
+        Button2 = New Button()
+        Button1 = New Button()
+        Button4 = New Button()
+        Button3 = New Button()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
         SuspendLayout()
         ' 
-        ' GroupBox1
+        ' Panel1
         ' 
-        GroupBox1.BackColor = SystemColors.ControlLight
-        GroupBox1.Controls.Add(MaskedTextBox1)
-        GroupBox1.Controls.Add(TextBox1)
-        GroupBox1.Location = New Point(149, 72)
-        GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(200, 224)
-        GroupBox1.TabIndex = 0
-        GroupBox1.TabStop = False
-        GroupBox1.Text = "calc"
+        Panel1.BackColor = Color.LightGray
+        Panel1.BorderStyle = BorderStyle.FixedSingle
+        Panel1.Controls.Add(TextBox1)
+        Panel1.Controls.Add(Panel2)
+        Panel1.Location = New Point(12, 12)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(336, 426)
+        Panel1.TabIndex = 0
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(12, 22)
+        TextBox1.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox1.Location = New Point(12, 4)
+        TextBox1.Margin = New Padding(4)
+        TextBox1.Multiline = True
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(188, 23)
-        TextBox1.TabIndex = 0
+        TextBox1.RightToLeft = RightToLeft.Yes
+        TextBox1.Size = New Size(309, 46)
+        TextBox1.TabIndex = 1
+        TextBox1.Text = "0"
         ' 
-        ' MaskedTextBox1
+        ' Panel2
         ' 
-        MaskedTextBox1.Location = New Point(0, 22)
-        MaskedTextBox1.Name = "MaskedTextBox1"
-        MaskedTextBox1.Size = New Size(100, 23)
-        MaskedTextBox1.TabIndex = 1
+        Panel2.BackColor = Color.MistyRose
+        Panel2.Controls.Add(Button24)
+        Panel2.Controls.Add(Button22)
+        Panel2.Controls.Add(Button21)
+        Panel2.Controls.Add(Button20)
+        Panel2.Controls.Add(Button19)
+        Panel2.Controls.Add(Button18)
+        Panel2.Controls.Add(Button17)
+        Panel2.Controls.Add(Button16)
+        Panel2.Controls.Add(Button15)
+        Panel2.Controls.Add(Button14)
+        Panel2.Controls.Add(Button13)
+        Panel2.Controls.Add(Button12)
+        Panel2.Controls.Add(Button11)
+        Panel2.Controls.Add(Button10)
+        Panel2.Controls.Add(Button9)
+        Panel2.Controls.Add(Button8)
+        Panel2.Controls.Add(Button7)
+        Panel2.Controls.Add(Button6)
+        Panel2.Controls.Add(Button5)
+        Panel2.Controls.Add(Button2)
+        Panel2.Controls.Add(Button1)
+        Panel2.Controls.Add(Button4)
+        Panel2.Controls.Add(Button3)
+        Panel2.Location = New Point(12, 57)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(309, 353)
+        Panel2.TabIndex = 0
+        ' 
+        ' Button24
+        ' 
+        Button24.BackColor = Color.WhiteSmoke
+        Button24.Font = New Font("Wingdings", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button24.Location = New Point(224, 36)
+        Button24.Name = "Button24"
+        Button24.Size = New Size(63, 46)
+        Button24.TabIndex = 25
+        Button24.Text = ""
+        Button24.UseVisualStyleBackColor = False
+        ' 
+        ' Button22
+        ' 
+        Button22.BackColor = Color.WhiteSmoke
+        Button22.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button22.Location = New Point(86, 35)
+        Button22.Name = "Button22"
+        Button22.Size = New Size(132, 46)
+        Button22.TabIndex = 23
+        Button22.Text = "CE"
+        Button22.TextAlign = ContentAlignment.TopCenter
+        Button22.UseVisualStyleBackColor = False
+        ' 
+        ' Button21
+        ' 
+        Button21.BackColor = Color.WhiteSmoke
+        Button21.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button21.Location = New Point(17, 36)
+        Button21.Name = "Button21"
+        Button21.Size = New Size(63, 46)
+        Button21.TabIndex = 22
+        Button21.Text = "^"
+        Button21.TextAlign = ContentAlignment.TopCenter
+        Button21.UseVisualStyleBackColor = False
+        ' 
+        ' Button20
+        ' 
+        Button20.BackColor = Color.WhiteSmoke
+        Button20.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button20.Location = New Point(224, 88)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(63, 46)
+        Button20.TabIndex = 21
+        Button20.Text = "/"
+        Button20.TextAlign = ContentAlignment.TopCenter
+        Button20.UseVisualStyleBackColor = False
+        ' 
+        ' Button19
+        ' 
+        Button19.BackColor = Color.WhiteSmoke
+        Button19.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button19.Location = New Point(155, 88)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(63, 46)
+        Button19.TabIndex = 20
+        Button19.Text = "%"
+        Button19.TextAlign = ContentAlignment.TopCenter
+        Button19.UseVisualStyleBackColor = False
+        ' 
+        ' Button18
+        ' 
+        Button18.BackColor = Color.WhiteSmoke
+        Button18.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button18.Location = New Point(86, 88)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(63, 46)
+        Button18.TabIndex = 19
+        Button18.Text = "x2"
+        Button18.TextAlign = ContentAlignment.TopCenter
+        Button18.UseVisualStyleBackColor = False
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = Color.WhiteSmoke
+        Button17.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button17.Location = New Point(17, 88)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(63, 46)
+        Button17.TabIndex = 18
+        Button17.Text = "1/x"
+        Button17.TextAlign = ContentAlignment.TopCenter
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = Color.WhiteSmoke
+        Button16.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button16.Location = New Point(224, 140)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(63, 46)
+        Button16.TabIndex = 17
+        Button16.Text = "x"
+        Button16.TextAlign = ContentAlignment.TopCenter
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = Color.WhiteSmoke
+        Button15.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button15.Location = New Point(155, 140)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(63, 46)
+        Button15.TabIndex = 16
+        Button15.Text = "9"
+        Button15.TextAlign = ContentAlignment.TopCenter
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = Color.WhiteSmoke
+        Button14.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button14.Location = New Point(86, 140)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(63, 46)
+        Button14.TabIndex = 15
+        Button14.Text = "8"
+        Button14.TextAlign = ContentAlignment.TopCenter
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.WhiteSmoke
+        Button13.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button13.Location = New Point(17, 140)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(63, 46)
+        Button13.TabIndex = 14
+        Button13.Text = "7"
+        Button13.TextAlign = ContentAlignment.TopCenter
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.WhiteSmoke
+        Button12.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button12.Location = New Point(224, 192)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(63, 46)
+        Button12.TabIndex = 13
+        Button12.Text = "-"
+        Button12.TextAlign = ContentAlignment.TopCenter
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.WhiteSmoke
+        Button11.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button11.Location = New Point(155, 192)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(63, 46)
+        Button11.TabIndex = 12
+        Button11.Text = "6"
+        Button11.TextAlign = ContentAlignment.TopCenter
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.WhiteSmoke
+        Button10.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button10.Location = New Point(86, 192)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(63, 46)
+        Button10.TabIndex = 11
+        Button10.Text = "5"
+        Button10.TextAlign = ContentAlignment.TopCenter
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.WhiteSmoke
+        Button9.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button9.Location = New Point(17, 192)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(63, 46)
+        Button9.TabIndex = 10
+        Button9.Text = "4"
+        Button9.TextAlign = ContentAlignment.TopCenter
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.WhiteSmoke
+        Button8.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button8.Location = New Point(224, 244)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(63, 46)
+        Button8.TabIndex = 9
+        Button8.Text = "+"
+        Button8.TextAlign = ContentAlignment.TopCenter
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.WhiteSmoke
+        Button7.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button7.Location = New Point(155, 244)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(63, 46)
+        Button7.TabIndex = 8
+        Button7.Text = "3"
+        Button7.TextAlign = ContentAlignment.TopCenter
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.WhiteSmoke
+        Button6.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button6.Location = New Point(86, 244)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(63, 46)
+        Button6.TabIndex = 7
+        Button6.Text = "2"
+        Button6.TextAlign = ContentAlignment.TopCenter
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.WhiteSmoke
+        Button5.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button5.Location = New Point(17, 244)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(63, 46)
+        Button5.TabIndex = 6
+        Button5.Text = "1"
+        Button5.TextAlign = ContentAlignment.TopCenter
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.WhiteSmoke
+        Button2.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.Location = New Point(86, 296)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(63, 46)
+        Button2.TabIndex = 5
+        Button2.Text = "0"
+        Button2.TextAlign = ContentAlignment.TopCenter
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.WhiteSmoke
+        Button1.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Location = New Point(17, 296)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(63, 46)
+        Button1.TabIndex = 4
+        Button1.Text = "+/-"
+        Button1.TextAlign = ContentAlignment.TopCenter
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.LightSkyBlue
+        Button4.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button4.Location = New Point(224, 296)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(63, 46)
+        Button4.TabIndex = 3
+        Button4.Text = "="
+        Button4.TextAlign = ContentAlignment.TopCenter
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.WhiteSmoke
+        Button3.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Button3.Location = New Point(155, 296)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(63, 46)
+        Button3.TabIndex = 2
+        Button3.Text = "."
+        Button3.TextAlign = ContentAlignment.TopCenter
+        Button3.UseVisualStyleBackColor = False
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Controls.Add(GroupBox1)
+        ClientSize = New Size(363, 450)
+        Controls.Add(Panel1)
         Name = "Form1"
         Text = "Form1"
-        GroupBox1.ResumeLayout(False)
-        GroupBox1.PerformLayout()
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
         ResumeLayout(False)
     End Sub
-
-    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button24 As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
 End Class
--- DESKTOP/CALCULATOR/CALC/CALC/Form1.vb
+++ DESKTOP/CALCULATOR/CALC/CALC/Form1.vb
@@ -1,9 +1,107 @@
 ﻿Public Class Form1
-    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
-
+    Dim a As Integer
+    Dim b As Integer
+    Dim sign As String
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TextBox1.Text = TextBox1.Text & 1
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text & 2
     End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text & 3
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        TextBox1.Text = TextBox1.Text & 4
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        TextBox1.Text = TextBox1.Text & 5
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        TextBox1.Text = TextBox1.Text & 6
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        TextBox1.Text = TextBox1.Text & 7
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        TextBox1.Text = TextBox1.Text & 8
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        TextBox1.Text = TextBox1.Text & 9
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text & 0
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        a = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "+"
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        a = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "-"
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        a = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "*"
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        a = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "/"
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        a = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "%"
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        a = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "^"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        b = TextBox1.Text
+        If sign = "+" Then
+            TextBox1.Text = a + b
+        ElseIf sign = "-" Then
+            TextBox1.Text = a - b
+        ElseIf sign = "*" Then
+            TextBox1.Text = a * b
+        ElseIf sign = "/" Then
+            TextBox1.Text = a / b
+        Else
+            TextBox1.Text = a Mod b
+        End If
+    End Sub
+
+    Private Sub Button22_Click(sender As Object, e As EventArgs) Handles Button22.Click
+        TextBox1.Clear()
+        TextBox1.Focus()
+    End Sub
+
 End Class
