d0b077f25eafeb3ad4189bf2492d15bd90e5decb
Juli-Savalia <3382julisavalia@gmail.com>
2024-02-02 09:47:13 +0530

    update

---
+++ DESKTOP/CALCULATOR/CALC/CALC/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        TextBox1 = New TextBox()
+        MaskedTextBox1 = New MaskedTextBox()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = SystemColors.ControlLight
+        GroupBox1.Controls.Add(MaskedTextBox1)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Location = New Point(149, 72)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(200, 224)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "calc"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 22)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(188, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' MaskedTextBox1
+        ' 
+        MaskedTextBox1.Location = New Point(0, 22)
+        MaskedTextBox1.Name = "MaskedTextBox1"
+        MaskedTextBox1.Size = New Size(100, 23)
+        MaskedTextBox1.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+End Class
---
+++ DESKTOP/CALCULATOR/CALC/CALC/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
